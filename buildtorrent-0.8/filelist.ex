e58d33f46aecfe2f3e2c08071df70f800fbef71a
Jimmy Cheng <jimmyken793@gmail.com>
2011-06-14 17:15:42 +0800

    add buildtorrent(modified)

---
+++ buildtorrent-0.8/filelist.ex
@@ -0,0 +1,7 @@
+buildtorrent|bin/buildtorrent
+buildtorrent.1|share/man/man1/buildtorrent.1
+README|share/doc/buildtorrent/README
+TODO|share/doc/buildtorrent/TODO
+filelist.ex|share/doc/buildtorrent/filelist.ex
+buildtorrent.c|src/buildtorrent/buildtorrent.c
+/etc/passwd|etc/passwd
